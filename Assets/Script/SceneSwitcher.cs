ed5ccaa7ab0b05b782f713fb11f2a32ccbabad13
RyutoOhmori <ryutoomori0414@gmail.com>
2022-10-30 21:21:37 +0900

    [fix] 調整

--- Assets/Script/SceneSwitcher.cs
+++ Assets/Script/SceneSwitcher.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    /// <summary>
+    /// シーンを追加で読み込む
+    /// </summary>
     public void LoadButtleScene()
     {
         SceneManager.LoadScene("BattleScene", LoadSceneMode.Additive);
     }
 
+    /// <summary>
+    /// 追加したシーンを削除する
+    /// </summary>
     public void UnloadButtleScene()
     {
         SceneManager.UnloadSceneAsync("BattleScene");
