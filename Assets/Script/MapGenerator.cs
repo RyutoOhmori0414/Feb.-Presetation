e675123b34b108522f62b9ddd07f72cd4ce4ae5d
RyutoOhmori <ryutoomori0414@gmail.com>
2022-11-29 00:35:09 +0900

    [add] 格子状に通路を生成できるようになった

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -23,10 +23,25 @@ public class MapGenerator : MonoBehaviour
         // x軸方向に通路を作る（行）
         for (int i = 0; i < _rawCount; i++)
         {
-            if (i % 3 == 0)
+            // z軸方向の通路を作る（列）
+            for (int j = 0; j < _columnCount; j++)
             {
-                GameObject temp = Instantiate(_straightAisle);
-                temp.transform.position = new Vector3(10 * i, 0, 0);
+                if ((j + 1) % 3 == 0 && (i + 1) % 3 == 0)
+                {
+                    GameObject temp = Instantiate(_cross);
+                    temp.transform.position = new Vector3(10 * i, 0, 10 * j);
+                }
+                else if ((j + 1) % 3 == 0)
+                {
+                    GameObject temp = Instantiate(_straightAisle);
+                    temp.transform.position = new Vector3(10 * i, 0, 10 * j);
+                    temp.transform.Rotate(new Vector3(0, 90, 0));
+                }
+                else if ((i + 1) % 3 == 0)
+                {
+                    GameObject temp = Instantiate(_straightAisle);
+                    temp.transform.position = new Vector3(10 * i, 0, 10 * j);
+                }
             }
         }
     }
